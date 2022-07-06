a1913e809bd0d6d9eb788145411908155f378376
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-07-06 13:53:04 +0900

    chore: 초기 환경 commit

---
+++ src/main/java/bandwagon/bandwagonback/BandwagonbackApplication.java
@@ -0,0 +1,13 @@
+package bandwagon.bandwagonback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandwagonbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandwagonbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bandwagon/bandwagonback/BandwagonbackApplicationTests.java
@@ -0,0 +1,13 @@
+package bandwagon.bandwagonback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandwagonbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
