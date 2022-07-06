ddd823773de657ab8ca074c649e60d59f1560196
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-07-06 17:40:44 +0900

    chore: PostgreSQL-JPA 연동 구현 및 User 도메인 추가-2

--- src/main/java/bandwagon/bandwagonback/domain/User.java
+++ src/main/java/bandwagon/bandwagonback/domain/User.java
@@ -1,2 +1,23 @@
-package bandwagon.bandwagonback.domain;public class User {
+package bandwagon.bandwagonback.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Getter
+public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+    private String nickname;
+    private String email;
+    private String password;
+    private Boolean gender; // 0 == Male, 1 == Female
+    private Date birthday;
 }
