80dc722fe3d358449f83d45ee1ac2fabfb13ae80
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-07-20 22:30:29 +0900

    feat: 활동 지역 제거 API 구현

--- src/main/java/bandwagon/bandwagonback/api/UserApiController.java
+++ src/main/java/bandwagon/bandwagonback/api/UserApiController.java
@@ -335,6 +335,24 @@ public class UserApiController {
         return ResponseEntity.ok().body(null);
     }
 
+    @Operation(description = "활동 지역 삭제")
+    @DeleteMapping("/api/users/{email}/areas/{area_id}")
+    public ResponseEntity<?> deleteArea(@PathVariable("email") String email, @PathVariable("area_id") Long area_id, HttpServletRequest request) {
+        String jwt = getJwtFromHeader(request);
+        String jwtEmail = jwtTokenUtil.extractUsername(jwt);
+
+        if (!jwtEmail.equals(email)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("User in token and user in URL is different"));
+        }
+
+        try {
+            areaService.deleteAreaFromUser(email, area_id);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+        }
+        return ResponseEntity.ok().body(null);
+    }
+
     private String getJwtFromHeader(HttpServletRequest request) {
         String authorizationHeader = request.getHeader("Authorization");
         return authorizationHeader.substring(7);
