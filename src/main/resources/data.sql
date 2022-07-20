c2d91a86023c5bf2c7d656d514b0263c94336197
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-07-20 16:40:27 +0900

    feat: Genre table init 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,13 @@ insert into positions(position) values('Bass Guitar');
 insert into positions(position) values('Keyboard');
 insert into positions(position) values('Vocal');
 insert into positions(position) values('Others');
+
+insert into genres(genre) values('Rock');
+insert into genres(genre) values('Metal');
+insert into genres(genre) values('POP');
+insert into genres(genre) values('J-POP');
+insert into genres(genre) values('K-POP');
+insert into genres(genre) values('Jazz');
+insert into genres(genre) values('Blues');
+insert into genres(genre) values('Punk');
+insert into genres(genre) values('Hip-Hop');
