d8fb76bcba389c1789f0ab181f9116c7969896c1
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-07-23 17:26:15 +0900

    feat: Band Repository 추가

---
+++ src/main/java/bandwagon/bandwagonback/repository/BandRepository.java
@@ -0,0 +1,7 @@
+package bandwagon.bandwagonback.repository;
+
+import bandwagon.bandwagonback.domain.Band;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BandRepository extends JpaRepository<Band, Long> {
+}
