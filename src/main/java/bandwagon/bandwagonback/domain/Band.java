18b2b5ba8df350f0dbdc136064971c9d1bb94bbc
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-07-23 16:40:24 +0900

    feat: 다대다 매핑 추가, 제거 메서드 추가

--- src/main/java/bandwagon/bandwagonback/domain/Band.java
+++ src/main/java/bandwagon/bandwagonback/domain/Band.java
@@ -40,4 +40,40 @@ public class Band {
             joinColumns = @JoinColumn(name = "band_id"),
             inverseJoinColumns = @JoinColumn(name = "area_id"))
     private List<Area> areas = new ArrayList<>();
+
+    // 포지션 추가
+    public void addPosition(Position position) {
+        this.positions.add(position);
+        position.getBands().add(this);
+    }
+
+    // 포지션 제거
+    public void removePosition(Position position) {
+        this.positions.remove(position);
+        position.getBands().remove(this);
+    }
+
+    // 선호 장르 추가
+    public void addGenre(Genre genre) {
+        this.genres.add(genre);
+        genre.getBands().add(this);
+    }
+
+    // 선호 장르 제거
+    public void removeGenre(Genre genre) {
+        this.genres.remove(genre);
+        genre.getBands().remove(this);
+    }
+
+    // 활동 지역 추가
+    public void addArea(Area area) {
+        this.areas.add(area);
+        area.getBands().add(this);
+    }
+
+    // 활동 지역 제거
+    public void removeArea(Area area) {
+        this.areas.remove(area);
+        area.getBands().remove(this);
+    }
 }
