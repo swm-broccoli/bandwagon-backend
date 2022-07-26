71bb86e6fd2d991de6ee860d4be3607e94213f09
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-07-26 12:03:52 +0900

    comment: Swagger Annotation 추가

--- src/main/java/bandwagon/bandwagonback/api/BandApiController.java
+++ src/main/java/bandwagon/bandwagonback/api/BandApiController.java
@@ -6,6 +6,7 @@ import bandwagon.bandwagonback.dto.ImageResponseDto;
 import bandwagon.bandwagonback.jwt.JwtUtil;
 import bandwagon.bandwagonback.service.BandMemberService;
 import bandwagon.bandwagonback.service.BandService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +29,7 @@ public class BandApiController {
     private final BandMemberService bandMemberService;
     private final JwtUtil jwtTokenUtil;
 
+    @Operation(description = "밴드 생성")
     @PostMapping("/api/band/create")
     public ResponseEntity<?> create(@RequestBody BandCreateForm bandCreateForm, HttpServletRequest request) {
         String jwt = getJwtFromHeader(request);
@@ -39,7 +41,8 @@ public class BandApiController {
         }
         return ResponseEntity.ok().body(null);
     }
-
+    
+    @Operation(description = "밴드 이름 수정")
     @PostMapping("/api/band/{band_id}/name")
     public ResponseEntity<?> editName(@PathVariable("band_id") Long bandId, @RequestBody EditNameForm editNameForm, HttpServletRequest request) {
         String jwt = getJwtFromHeader(request);
@@ -52,6 +55,7 @@ public class BandApiController {
         return ResponseEntity.ok().body(null);
     }
 
+    @Operation(description = "밴드 소개 수정")
     @PostMapping("/api/band/{band_id}/description")
     public ResponseEntity<?> editDescription(@PathVariable("band_id") Long bandId, @RequestBody EditDescriptionForm editDescriptionForm, HttpServletRequest request) {
         String jwt = getJwtFromHeader(request);
@@ -64,6 +68,7 @@ public class BandApiController {
         return ResponseEntity.ok().body(null);
     }
 
+    @Operation(description = "밴드 아바타 수정")
     @PostMapping("/api/band/{band_id}/avatar")
     public ResponseEntity<?> postBandAvatar(@PathVariable("band_id") Long bandId, @RequestParam("image") MultipartFile multipartFile, HttpServletRequest request) {
         String jwt = getJwtFromHeader(request);
@@ -77,6 +82,7 @@ public class BandApiController {
     }
 
     // 추후엔 유저 검색해서 찾고 합류 신청 보내면 바로 합류 되는게 아니라 수락 해야 join 되도록
+    @Operation(description = "밴드 멤버 추가")
     @PostMapping("/api/band/{band_id}/member")
     public ResponseEntity<?> addMember(@PathVariable("band_id") Long bandId, @RequestBody AddMemberForm addMemberForm, HttpServletRequest request) {
         String jwt = getJwtFromHeader(request);
@@ -89,6 +95,7 @@ public class BandApiController {
         }
     }
 
+    @Operation(description = "밴드 멤버 제거")
     @DeleteMapping("/api/band/{band_id}/member/{band_member_id}")
     public ResponseEntity<?> removeMember(@PathVariable("band_id") Long bandId, @PathVariable("band_member_id") Long bandMemberId, HttpServletRequest request) {
         String jwt = getJwtFromHeader(request);
@@ -101,6 +108,7 @@ public class BandApiController {
         }
     }
 
+    @Operation(description = "밴드 멤버 포지션 추가")
     @PostMapping("/api/band/{band_id}/member/{band_member_id}/positions/{position_id}")
     public ResponseEntity<?> addPositionToMember(@PathVariable("band_id") Long bandId, @PathVariable("band_member_id") Long bandMemberId, @PathVariable("position_id") Long positionId, HttpServletRequest request) {
         String jwt = getJwtFromHeader(request);
@@ -112,7 +120,8 @@ public class BandApiController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
         }
     }
-
+    
+    @Operation(description = "밴드 멤버 포지션 제거")
     @DeleteMapping("/api/band/{band_id}/member/{band_member_id}/positions/{position_id}")
     public ResponseEntity<?> deletePositionFromMember(@PathVariable("band_id") Long bandId, @PathVariable("band_member_id") Long bandMemberId, @PathVariable("position_id") Long positionId, HttpServletRequest request) {
         String jwt = getJwtFromHeader(request);
