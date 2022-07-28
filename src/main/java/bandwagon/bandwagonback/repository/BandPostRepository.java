37a373b5f7b350b79dae99421000881bec5c684c
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-07-28 14:30:02 +0900

    refactor: BandPost에서 Post로 변경

--- src/main/java/bandwagon/bandwagonback/repository/BandPostRepository.java
+++
@@ -1,7 +0,0 @@
-package bandwagon.bandwagonback.repository;
-
-import bandwagon.bandwagonback.domain.post.BandPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BandPostRepository extends JpaRepository<BandPost, Long> {
-}
---
+++ src/main/java/bandwagon/bandwagonback/service/PostService.java
@@ -0,0 +1,18 @@
+package bandwagon.bandwagonback.service;
+
+import bandwagon.bandwagonback.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+
+}
