f812591ce4148b61347af65593e377e2b75c5906
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-01 13:46:29 +0900

    feat: Band Post edit하는 API 구현

--- src/main/java/bandwagon/bandwagonback/api/BandPostApiController.java
+++ src/main/java/bandwagon/bandwagonback/api/BandPostApiController.java
@@ -42,6 +42,23 @@ public class BandPostApiController {
         }
     }
 
+    @PutMapping("/api/band/post")
+    public ResponseEntity<?> editBandPost(@RequestBody PostDto postDto, HttpServletRequest request) {
+        String jwt = getJwtFromHeader(request);
+        String email = jwtTokenUtil.extractUsername(jwt);
+        try {
+            Long bandId = bandMemberService.getBandIdByUserEmail(email);
+            BandPost bandPost = postService.getBandPostByPostId(postDto.getId());
+            if (!bandPost.getBand().getId().equals(bandId)) {
+                throw new Exception("로그인 한 유저와 request로 제공된 post의 band가 일치하지 않습니다!");
+            }
+            postService.editPost(postDto);
+            return ResponseEntity.ok().body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+        }
+    }
+
     @DeleteMapping("/api/band/post/{post_id}")
     public ResponseEntity<?> deleteBandPost(@PathVariable("post_id") Long postId, HttpServletRequest request) {
         String jwt = getJwtFromHeader(request);
