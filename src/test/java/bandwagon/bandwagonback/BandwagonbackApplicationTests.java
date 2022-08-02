202b441b6127fe999abe4ad650902ca0bf5cf9e7
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-02 14:40:59 +0900

    fix: test 파일에 불필요한 Annotation 제거

--- src/test/java/bandwagon/bandwagonback/BandwagonbackApplicationTests.java
+++ src/test/java/bandwagon/bandwagonback/BandwagonbackApplicationTests.java
@@ -1,13 +1,6 @@
 package bandwagon.bandwagonback;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BandwagonbackApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
