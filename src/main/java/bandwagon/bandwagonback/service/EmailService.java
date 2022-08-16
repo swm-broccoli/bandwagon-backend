889a715be5e01ae2f4f5867b21ecddf452962573
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-16 15:08:11 +0900

    feat: 이메일 전송 service 구현

---
+++ src/main/java/bandwagon/bandwagonback/service/EmailService.java
@@ -0,0 +1,24 @@
+package bandwagon.bandwagonback.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@bandwagon.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
