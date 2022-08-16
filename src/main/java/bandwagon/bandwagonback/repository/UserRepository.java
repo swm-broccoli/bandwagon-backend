44f0e5957b94311e4099781b0688aa33fdb38b0e
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-16 13:59:44 +0900

    feat: 유저 이메일 찾기 서비스 추가

--- src/main/java/bandwagon/bandwagonback/repository/UserRepository.java
+++ src/main/java/bandwagon/bandwagonback/repository/UserRepository.java
@@ -12,5 +12,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByNameAndBirthday(String name, LocalDate birthday);
+    List<User> findAllByNameAndBirthday(String name, LocalDate birthday);
 }
