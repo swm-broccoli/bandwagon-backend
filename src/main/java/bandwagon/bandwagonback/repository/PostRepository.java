b9cf749fc3d8b2962efdc854a7928548474d64df
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-18 14:35:32 +0900

    feat: 찜한 글 페이지 service 추가

--- src/main/java/bandwagon/bandwagonback/repository/PostRepository.java
+++ src/main/java/bandwagon/bandwagonback/repository/PostRepository.java
@@ -1,7 +1,10 @@
 package bandwagon.bandwagonback.repository;
 
 import bandwagon.bandwagonback.domain.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>{
+    Page<Post> findAllByLikingUsers_email(String email, PageRequest pageRequest);
 }
