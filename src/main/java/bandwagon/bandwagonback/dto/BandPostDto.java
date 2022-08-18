7486ea3a68441fadea05adab2c35c431e1387c4b
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-18 14:13:22 +0900

    refactor: User, Band Post Dto들 PostDto 상속 받게 변경

--- src/main/java/bandwagon/bandwagonback/dto/BandPostDto.java
+++ src/main/java/bandwagon/bandwagonback/dto/BandPostDto.java
@@ -3,14 +3,11 @@ package bandwagon.bandwagonback.dto;
 import bandwagon.bandwagonback.domain.User;
 import bandwagon.bandwagonback.domain.post.BandPost;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class BandPostDto {
-    private Long id;
-    private String title;
-    private String body;
-    private String dtype;
-    private Long bandId;
+@EqualsAndHashCode(callSuper = true)
+public class BandPostDto extends PostDto{
     private String bandName;
     private String bandAvatarUrl;
     private Boolean isLiked;
@@ -18,11 +15,7 @@ public class BandPostDto {
     public BandPostDto() {}
 
     public BandPostDto(BandPost post) {
-        this.id = post.getId();
-        this.title = post.getTitle();
-        this.body = post.getBody();
-        this.dtype = post.getDtype();
-        this.bandId = post.getBand().getId();
+        super(post);
         this.bandName = post.getBand().getName();
         this.bandAvatarUrl = post.getBand().getAvatarUrl();
         this.isLiked = false;
