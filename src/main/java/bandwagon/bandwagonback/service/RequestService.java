e45c18bc26986f505604386e6df85ec21cb8338f
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-21 16:35:15 +0900

    feat: Apply Request 거절하는 메서드 추가

--- src/main/java/bandwagon/bandwagonback/service/RequestService.java
+++ src/main/java/bandwagon/bandwagonback/service/RequestService.java
@@ -1,10 +1,9 @@
 package bandwagon.bandwagonback.service;
 
-import bandwagon.bandwagonback.domain.Band;
-import bandwagon.bandwagonback.domain.Request;
-import bandwagon.bandwagonback.domain.RequestType;
-import bandwagon.bandwagonback.domain.User;
+import bandwagon.bandwagonback.domain.*;
+import bandwagon.bandwagonback.repository.BandMemberRepository;
 import bandwagon.bandwagonback.repository.RequestRepository;
+import bandwagon.bandwagonback.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class RequestService {
 
     private final RequestRepository requestRepository;
+    private final UserRepository userRepository;
+    private final BandMemberRepository bandMemberRepository;
 
     @Transactional
     public void createRequest(User user, Band band, RequestType requestType) {
@@ -25,5 +26,21 @@ public class RequestService {
         user.addRequest(request);
         band.addRequest(request);
         requestRepository.save(request);
+    }
+
+    @Transactional
+    public void declineApplyRequest(String email, Long requestId) throws Exception {
+        Request request = requestRepository.findById(requestId).orElse(null);
+        if (request == null || !request.getType().equals(RequestType.APPLY)) {
+            throw new Exception("Specified Request is Not Valid");
+        }
+        BandMember bandMember = bandMemberRepository.findFirstByMember_emailAndBand_id(email, request.getBand().getId());
+        if (bandMember == null) {
+            throw new Exception("Declining User is not Part of the Band");
+        }
+        if (!bandMember.getIsFrontman()) {
+            throw new Exception("Declining User is not frontman and cannot decline this Request!");
+        }
+        requestRepository.delete(request);
     }
 }
