19da274cccb9dfe3cd19938e8a1ed8fd6d0ed176
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-23 14:30:06 +0900

    refactor: 밴드 초대 수락 알림 시점 변경

--- src/main/java/bandwagon/bandwagonback/service/RequestService.java
+++ src/main/java/bandwagon/bandwagonback/service/RequestService.java
@@ -127,11 +127,12 @@ public class RequestService {
         if (user.getBandMember() != null) {
             throw new Exception("이미 밴드에 속한 유저입니다");
         }
+        // 자기 자신은 알림 받지 않게 밴드에 추가 되기 직전에 밴드 맴버들에게 알림 발송
+        notificationService.createUserToBand(user, request.getBand(), NotificationType.INVITE_ACCEPT);
         BandMember newMember = new BandMember(user, false);
         request.getBand().addBandMember(newMember);
         bandMemberRepository.save(newMember);
         requestRepository.delete(request);
-        notificationService.createUserToBand(user, request.getBand(), NotificationType.INVITE_ACCEPT);
     }
 
     @Transactional
