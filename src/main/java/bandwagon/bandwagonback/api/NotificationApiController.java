ccc45e048843311b59e9f500bea6f2a0cc89c4f1
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-23 16:47:48 +0900

    feat: 안 읽은 Notification count API 구현

---
+++ src/main/java/bandwagon/bandwagonback/api/NotificationApiController.java
@@ -0,0 +1,51 @@
+package bandwagon.bandwagonback.api;
+
+import bandwagon.bandwagonback.domain.User;
+import bandwagon.bandwagonback.dto.ErrorResponse;
+import bandwagon.bandwagonback.dto.SimpleCountResponse;
+import bandwagon.bandwagonback.jwt.JwtUtil;
+import bandwagon.bandwagonback.service.NotificationService;
+import bandwagon.bandwagonback.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Tag(name = "PostApiController")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class NotificationApiController {
+
+    private final NotificationService notificationService;
+    private final UserService userService;
+    private final JwtUtil jwtTokenUtil;
+
+    @Operation(description = "안읽은 알림 수 조회")
+    @GetMapping("/api/notifications/count")
+    public ResponseEntity<?> getUnreadNotiCount(HttpServletRequest request) {
+        String jwt = getJwtFromHeader(request);
+        String email = jwtTokenUtil.extractUsername(jwt);
+        try {
+            User user = userService.findOneByEmail(email);
+            long count = notificationService.getUnreadNotificationCount(user);
+            return ResponseEntity.ok(new SimpleCountResponse(count));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+        }
+    }
+
+    private String getJwtFromHeader(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        return authorizationHeader.substring(7);
+    }
+}
