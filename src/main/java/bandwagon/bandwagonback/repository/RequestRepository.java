380eeef516b5705d249c9dfa7f6a6b12650a260c
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-24 14:53:26 +0900

    feat: Request 조회 query fetch join으로 변경

--- src/main/java/bandwagon/bandwagonback/repository/RequestRepository.java
+++ src/main/java/bandwagon/bandwagonback/repository/RequestRepository.java
@@ -5,10 +5,14 @@ import bandwagon.bandwagonback.domain.Request;
 import bandwagon.bandwagonback.domain.RequestType;
 import bandwagon.bandwagonback.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RequestRepository extends JpaRepository<Request, Long> {
-    List<Request> findAllByUserAndType(User user, RequestType type);
-    List<Request> findAllByBandAndType(Band band, RequestType type);
+    @Query("select distinct r from Request r left join fetch r.user left join fetch r.band left join fetch r.bandPost where r.user = :user and r.type = :type")
+    List<Request> findAllByUserAndType(@Param("user") User user, @Param("type") RequestType type);
+    @Query("select distinct r from Request r left join fetch r.user left join fetch r.band left join fetch r.bandPost where r.band = :band and r.type = :type")
+    List<Request> findAllByBandAndType(@Param("band") Band band, @Param("type") RequestType type);
 }
