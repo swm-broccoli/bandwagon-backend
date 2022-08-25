bf00da82f646c6199379ce58df93c3e0b5d59abb
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-25 17:15:09 +0900

    feat: ErrorCode Interface 추가

---
+++ src/main/java/bandwagon/bandwagonback/dto/exception/ErrorCode.java
@@ -0,0 +1,10 @@
+package bandwagon.bandwagonback.dto.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+    String name();
+    HttpStatus getHttpStatus();
+    String getMessage();
+}
