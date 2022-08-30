cf05dde621c56b91b1187b564a77768f44ea2bf9
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-30 18:52:01 +0900

    feat: BandGig Not Found Exception Class 추가

---
+++ src/main/java/bandwagon/bandwagonback/dto/exception/notfound/BandGigNotFoundException.java
@@ -0,0 +1,7 @@
+package bandwagon.bandwagonback.dto.exception.notfound;
+
+public class BandGigNotFoundException extends NotFoundException {
+    public BandGigNotFoundException() {
+        super("밴드 공연기록을");
+    }
+}
