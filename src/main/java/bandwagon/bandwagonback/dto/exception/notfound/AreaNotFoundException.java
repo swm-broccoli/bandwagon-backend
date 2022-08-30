c49f84df188b07a3d1770a9121fa18f6210bc640
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-30 18:47:02 +0900

    feat: Area Not Found Exception Class 추가

---
+++ src/main/java/bandwagon/bandwagonback/dto/exception/notfound/AreaNotFoundException.java
@@ -0,0 +1,7 @@
+package bandwagon.bandwagonback.dto.exception.notfound;
+
+public class AreaNotFoundException extends NotFoundException {
+    public AreaNotFoundException() {
+        super("지역을");
+    }
+}
