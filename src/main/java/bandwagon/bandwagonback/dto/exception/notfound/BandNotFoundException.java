536f1b8155ec451403acec7612e76b7648a3b4f4
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-30 18:46:01 +0900

    feat: Band Not Found Exception Class 추가

---
+++ src/main/java/bandwagon/bandwagonback/dto/exception/notfound/BandNotFoundException.java
@@ -0,0 +1,7 @@
+package bandwagon.bandwagonback.dto.exception.notfound;
+
+public class BandNotFoundException extends NotFoundException {
+    public BandNotFoundException() {
+        super("밴드를");
+    }
+}
