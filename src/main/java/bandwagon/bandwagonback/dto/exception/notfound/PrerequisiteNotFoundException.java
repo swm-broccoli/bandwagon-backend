60eff989fe17580e847dd143b8b82458602e04be
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-30 18:56:32 +0900

    feat: Prerequisite Not Found Exception Class 추가

---
+++ src/main/java/bandwagon/bandwagonback/dto/exception/notfound/PrerequisiteNotFoundException.java
@@ -0,0 +1,7 @@
+package bandwagon.bandwagonback.dto.exception.notfound;
+
+public class PrerequisiteNotFoundException extends NotFoundException {
+    public PrerequisiteNotFoundException() {
+        super("지원조건을");
+    }
+}
