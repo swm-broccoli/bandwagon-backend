60770f75ce7438d8b5b4d8ffb92cd55c8ec062db
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-08-30 18:45:03 +0900

    feat: User Not Found Exception Class 추가

--- src/main/java/bandwagon/bandwagonback/dto/exception/notfound/NotFoundException.java
+++ src/main/java/bandwagon/bandwagonback/dto/exception/notfound/NotFoundException.java
@@ -1,8 +1,9 @@
-package bandwagon.bandwagonback.dto.exception;
+package bandwagon.bandwagonback.dto.exception.notfound;
 
+import bandwagon.bandwagonback.dto.exception.CustomException;
 import org.springframework.http.HttpStatus;
 
-public class NotFoundException extends CustomException{
+public class NotFoundException extends CustomException {
 
     public NotFoundException(String message) {
         super(message + " 찾을 수 없습니다");
