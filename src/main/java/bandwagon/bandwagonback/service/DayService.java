ea064b99aa7b95aa48c09da1788989dbe39c9e43
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-05 16:14:25 +0900

    feat: Day Service에 NotFoundException 적용

--- src/main/java/bandwagon/bandwagonback/service/DayService.java
+++ src/main/java/bandwagon/bandwagonback/service/DayService.java
@@ -3,6 +3,8 @@ package bandwagon.bandwagonback.service;
 import bandwagon.bandwagonback.domain.Band;
 import bandwagon.bandwagonback.domain.Day;
 import bandwagon.bandwagonback.domain.Genre;
+import bandwagon.bandwagonback.dto.exception.notfound.BandNotFoundException;
+import bandwagon.bandwagonback.dto.exception.notfound.DayNotFoundException;
 import bandwagon.bandwagonback.repository.BandMemberRepository;
 import bandwagon.bandwagonback.repository.BandRepository;
 import bandwagon.bandwagonback.repository.DayRepository;
@@ -25,14 +27,14 @@ public class DayService {
     public void addDayToBand(String email, Long bandId, Long dayId) throws Exception {
         Band band = bandRepository.findById(bandId).orElse(null);
         if (band == null) {
-            throw new Exception("Band does not exist!");
+            throw new BandNotFoundException();
         }
         if (bandMemberRepository.findFirstByMember_emailAndBand_id(email, bandId) == null) {
             throw new Exception("해당 밴드에 속하지 않은 유저입니다!");
         }
         Day day = dayRepository.findById(dayId).orElse(null);
         if (day == null) {
-            throw new Exception("Day does not exist!");
+            throw new DayNotFoundException();
         }
         if (band.getDays().contains(day)) {
             log.info("User already has day: {}", day.getDay());
@@ -45,14 +47,14 @@ public class DayService {
     public void deleteDayFromBand(String email, Long bandId, Long dayId) throws Exception {
         Band band = bandRepository.findById(bandId).orElse(null);
         if (band == null) {
-            throw new Exception("Band does not exist!");
+            throw new BandNotFoundException();
         }
         if (bandMemberRepository.findFirstByMember_emailAndBand_id(email, bandId) == null) {
             throw new Exception("해당 밴드에 속하지 않은 유저입니다!");
         }
         Day day = dayRepository.findById(dayId).orElse(null);
         if (day == null) {
-            throw new Exception("Day does not exist!");
+            throw new DayNotFoundException();
         }
         band.removeDay(day);
     }
