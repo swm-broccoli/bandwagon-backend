ec57c19bbcb06f7248f0832ed9b2a08ef0ac8f70
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-05 16:20:22 +0900

    feat: UserPerformance Service에 NotFoundException 적용

--- src/main/java/bandwagon/bandwagonback/service/UserPerformanceService.java
+++ src/main/java/bandwagon/bandwagonback/service/UserPerformanceService.java
@@ -3,6 +3,8 @@ package bandwagon.bandwagonback.service;
 import bandwagon.bandwagonback.domain.User;
 import bandwagon.bandwagonback.domain.UserPerformance;
 import bandwagon.bandwagonback.dto.PerformanceDto;
+import bandwagon.bandwagonback.dto.exception.notfound.UserNotFoundException;
+import bandwagon.bandwagonback.dto.exception.notfound.UserPerformanceNotFoundException;
 import bandwagon.bandwagonback.repository.UserPerformanceRepository;
 import bandwagon.bandwagonback.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,10 +27,10 @@ public class UserPerformanceService {
 
     // 신규 연주기록 저장
     @Transactional
-    public void saveUserPerformance(String email, PerformanceDto performanceDto) throws Exception {
+    public void saveUserPerformance(String email, PerformanceDto performanceDto) {
         User user = userRepository.findByEmail(email).orElse(null);
         if (user == null) {
-            throw new Exception("User does not exist!");
+            throw new UserNotFoundException();
         }
         UserPerformance userPerformance = new UserPerformance(performanceDto);
         user.addUserPerformance(userPerformance);
@@ -40,11 +42,11 @@ public class UserPerformanceService {
     public void deleteUserPerformance(String email, Long userPerformanceId) throws Exception {
         User user = userRepository.findByEmail(email).orElse(null);
         if (user == null) {
-            throw new Exception("User does not exist!");
+            throw new UserNotFoundException();
         }
         UserPerformance userPerformance = userPerformanceRepository.findById(userPerformanceId).orElse(null);
         if (userPerformance == null) {
-            throw new Exception("User Performance does not exist!");
+            throw new UserPerformanceNotFoundException();
         }
         if (userPerformance.getUser() != user) {
             throw new Exception("Owner of performance is not inputted user!");
@@ -57,11 +59,11 @@ public class UserPerformanceService {
     public void updateUserPerformance(String email, Long userPerformanceId, PerformanceDto performanceDto) throws Exception{
         User user = userRepository.findByEmail(email).orElse(null);
         if (user == null) {
-            throw new Exception("User does not exist!");
+            throw new UserNotFoundException();
         }
         UserPerformance userPerformance = userPerformanceRepository.findById(userPerformanceId).orElse(null);
         if(userPerformance == null) {
-            throw new Exception("User Performance does not exist!");
+            throw new UserPerformanceNotFoundException();
         }
         if (userPerformance.getUser() != user) {
             throw new Exception("Owner of performance is not inputted user!");
