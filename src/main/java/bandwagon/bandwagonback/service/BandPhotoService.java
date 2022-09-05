b3105e3d7bcd33df1230db7dc4e0b19fae5d298d
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-05 17:56:12 +0900

    feat: BandPhotoService에 NotOfException 적용

--- src/main/java/bandwagon/bandwagonback/service/BandPhotoService.java
+++ src/main/java/bandwagon/bandwagonback/service/BandPhotoService.java
@@ -3,6 +3,8 @@ package bandwagon.bandwagonback.service;
 import bandwagon.bandwagonback.domain.Band;
 import bandwagon.bandwagonback.domain.BandPhoto;
 import bandwagon.bandwagonback.dto.exception.notfound.BandNotFoundException;
+import bandwagon.bandwagonback.dto.exception.notof.BandPhotoNotOfBandException;
+import bandwagon.bandwagonback.dto.exception.notof.UserNotOfBandException;
 import bandwagon.bandwagonback.repository.BandMemberRepository;
 import bandwagon.bandwagonback.repository.BandPhotoRepository;
 import bandwagon.bandwagonback.repository.BandRepository;
@@ -13,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Slf4j
 @Service
@@ -26,7 +30,7 @@ public class BandPhotoService {
     private final S3Uploader s3Uploader;
 
     @Transactional
-    public String addPhotoToBand(String email, Long bandId, MultipartFile multipartFile) throws Exception {
+    public String addPhotoToBand(String email, Long bandId, MultipartFile multipartFile) throws IOException {
         Band band = confirmUserInBand(email, bandId);
         String imgUrl = s3Uploader.upload(multipartFile, "band/photo");
         BandPhoto bandPhoto = new BandPhoto(imgUrl);
@@ -36,24 +40,24 @@ public class BandPhotoService {
     }
 
     @Transactional
-    public void removePhotoFromBand(String email, Long bandId, Long bandPhotoId) throws Exception {
+    public void removePhotoFromBand(String email, Long bandId, Long bandPhotoId) throws URISyntaxException {
         Band band = confirmUserInBand(email, bandId);
         BandPhoto bandPhoto = bandPhotoRepository.findById(bandPhotoId).orElse(null);
         if (bandPhoto.getBand() != band) {
-            throw new Exception("해당 밴드의 사진이 아닙니다!");
+            throw new BandPhotoNotOfBandException();
         }
         s3Uploader.deleteFromS3(bandPhoto.getImgUrl().replace(File.separatorChar, '/'));
         bandPhotoRepository.deleteById(bandPhotoId);
     }
 
     @Transactional
-    public Band confirmUserInBand(String email, Long bandId) throws Exception {
+    public Band confirmUserInBand(String email, Long bandId) {
         Band band = bandRepository.findById(bandId).orElse(null);
         if(band == null) {
             throw new BandNotFoundException();
         }
         if (bandMemberRepository.findFirstByMember_emailAndBand_id(email, bandId) == null) {
-            throw new Exception("해당 밴드에 속하지 않은 유저입니다!");
+            throw new UserNotOfBandException();
         }
         return band;
     }
