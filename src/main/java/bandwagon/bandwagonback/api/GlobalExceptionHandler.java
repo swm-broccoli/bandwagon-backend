579eaf2ba1f2a20027d5d8daf661d1cfe70909d1
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-05 16:43:29 +0900

    feat: UserApiController에 NotFoundException 적용

--- src/main/java/bandwagon/bandwagonback/api/GlobalExceptionHandler.java
+++ src/main/java/bandwagon/bandwagonback/api/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package bandwagon.bandwagonback.api;
 import bandwagon.bandwagonback.dto.ErrorResponse;
 import bandwagon.bandwagonback.dto.exception.notfound.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -16,5 +18,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
         log.error("handleNotFoundException", e);
         return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentialsException(BadCredentialsException e) {
+        log.error("handleBadCredentialsException", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("비밀번호가 올바르지 않습니다!"));
     }
 }
--- src/main/java/bandwagon/bandwagonback/api/UserApiController.java
+++ src/main/java/bandwagon/bandwagonback/api/UserApiController.java
@@ -41,29 +41,16 @@ public class UserApiController {
     @Operation(description = "로그인")
     @PostMapping("/api/login")
     public ResponseEntity<?> login(@RequestBody LoginForm form) {
-        try {
-            log.info("Login init...");
-            User user = userService.findOneByEmail(form.getEmail());
-            if(user.getIsSocial()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("가입되지 않은 회원입니다!"));
-            }
-            // 로그인 authentication 통과 못할 시 BadCredentialsException
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(form.getEmail(), form.getPassword()));
-
-            Map<String, String> tokens = jwtTokenUtil.generateToken(new UserTokenDto(user.getEmail(), false));
-
-
-            log.info("Logging in User: {}", user.getEmail());
-
-            return ResponseEntity.ok(new LoginResponse(tokens.get("accessToken"), tokens.get("refreshToken"), user.getNickname(), jwtTokenUtil.extractExpiration(tokens.get("accessToken"))));
-
-        } catch (BadCredentialsException e) {
-            log.error("Wrong Password");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("비밀번호가 올바르지 않습니다!"));
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+        log.info("Login init...");
+        User user = userService.findOneByEmail(form.getEmail());
+        if(user.getIsSocial()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("가입되지 않은 회원입니다!"));
         }
+        // 로그인 authentication 통과 못할 시 BadCredentialsException
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(form.getEmail(), form.getPassword()));
+        Map<String, String> tokens = jwtTokenUtil.generateToken(new UserTokenDto(user.getEmail(), false));
+        log.info("Logging in User: {}", user.getEmail());
+        return ResponseEntity.ok(new LoginResponse(tokens.get("accessToken"), tokens.get("refreshToken"), user.getNickname(), jwtTokenUtil.extractExpiration(tokens.get("accessToken"))));
     }
 
     @Operation(description = "회원 탈퇴")
@@ -83,25 +70,20 @@ public class UserApiController {
     @Operation(description = "Refresh Token으로 token 재발급")
     @PostMapping("/api/refresh")
     public ResponseEntity<?> refresh(@RequestBody RefreshRequest request) {
-        try {
-            String refreshToken = request.getRefreshToken();
-            // 여기 뭔가 모순이 있는듯, 토큰에서 추출한 이메일로 토큰 validation 검사를 함
-            String username = jwtTokenUtil.extractUsername(refreshToken);
-            Boolean isSocial = jwtTokenUtil.extractIsSocial(refreshToken);
-            UserTokenDto userTokenDto = new UserTokenDto(username, isSocial);
-            if(jwtTokenUtil.validateToken(refreshToken, username)) {
-                if(!jwtTokenUtil.extractIsRefresh(refreshToken)) {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Refresh Token이 아닌 AccessToken으로 refresh 시도"));
-                }
-                Map<String, String> tokens = jwtTokenUtil.generateToken(userTokenDto);
-                User user = userService.findOneByEmail(username);
-                return ResponseEntity.ok(new LoginResponse(tokens.get("accessToken"), tokens.get("refreshToken"), user.getNickname(), jwtTokenUtil.extractExpiration(tokens.get("accessToken"))));
+        String refreshToken = request.getRefreshToken();
+        // 여기 뭔가 모순이 있는듯, 토큰에서 추출한 이메일로 토큰 validation 검사를 함
+        String username = jwtTokenUtil.extractUsername(refreshToken);
+        Boolean isSocial = jwtTokenUtil.extractIsSocial(refreshToken);
+        UserTokenDto userTokenDto = new UserTokenDto(username, isSocial);
+        if(jwtTokenUtil.validateToken(refreshToken, username)) {
+            if(!jwtTokenUtil.extractIsRefresh(refreshToken)) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Refresh Token이 아닌 AccessToken으로 refresh 시도"));
             }
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Refresh Token 만료! 로그인 필요"));
-        } catch (Exception e){
-            log.error(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+            Map<String, String> tokens = jwtTokenUtil.generateToken(userTokenDto);
+            User user = userService.findOneByEmail(username);
+            return ResponseEntity.ok(new LoginResponse(tokens.get("accessToken"), tokens.get("refreshToken"), user.getNickname(), jwtTokenUtil.extractExpiration(tokens.get("accessToken"))));
         }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Refresh Token 만료! 로그인 필요"));
     }
 
     @Operation(description = "회원가입")
@@ -133,13 +115,8 @@ public class UserApiController {
     @Operation(description = "유저 이메일 찾기")
     @PostMapping("/api/find/email")
     public ResponseEntity<?> findUserEmail(@RequestBody FindUserEmailRequest request) {
-        try {
-            FindUserEmailDto findUserEmailDto = userService.findUserEmail(request);
-            return ResponseEntity.ok(findUserEmailDto);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
-        }
+        FindUserEmailDto findUserEmailDto = userService.findUserEmail(request);
+        return ResponseEntity.ok(findUserEmailDto);
     }
 
     @Operation(description = "유저 비밀번호 찾기")
@@ -157,15 +134,10 @@ public class UserApiController {
     @Operation(description = "유저 기본 정보 조회")
     @GetMapping("/api/users")
     public ResponseEntity<?> getUserInfo(HttpServletRequest request) {
-        try {
-            String jwt = getJwtFromHeader(request);
-            String email = jwtTokenUtil.extractUsername(jwt);
-            User user = userService.findOneByEmail(email);
-            return ResponseEntity.ok(new UserEditDto(user));
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
-        }
+        String jwt = getJwtFromHeader(request);
+        String email = jwtTokenUtil.extractUsername(jwt);
+        User user = userService.findOneByEmail(email);
+        return ResponseEntity.ok(new UserEditDto(user));
     }
 
     @Operation(description = "유저 기본 정보/비밀번호 수정")
