c4b9147744530b52129dca2ccf584a1b00d0432e
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-07 10:53:30 +0900

    feat: User Post에 들어가는 tag 수정

--- src/main/java/bandwagon/bandwagonback/dto/UserPostDto.java
+++ src/main/java/bandwagon/bandwagonback/dto/UserPostDto.java
@@ -2,16 +2,20 @@ package bandwagon.bandwagonback.dto;
 
 import bandwagon.bandwagonback.domain.User;
 import bandwagon.bandwagonback.domain.post.UserPost;
+import bandwagon.bandwagonback.dto.subdto.IdNameForm;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class UserPostDto extends PostDto {
     private String nickname;
     private String userAvatarUrl;
-    //TODO: 나중엔 필요한 테그 정보만 담게 변경
-    private MyPageDto tagInfo;
+    private List<IdNameForm> tagInfo;
 
     public UserPostDto() {}
 
@@ -19,7 +23,7 @@ public class UserPostDto extends PostDto {
         super(post);
         this.nickname = post.getUser().getNickname();
         this.userAvatarUrl = post.getUser().getUserInfo().getAvatarUrl();
-        this.tagInfo = new MyPageDto(post.getUser());
+        this.tagInfo = post.getUser().getPositions().stream().map(IdNameForm::new).collect(Collectors.toList());
     }
 
     public static UserPostDto makeUserPostDto(UserPost post, User user) {
