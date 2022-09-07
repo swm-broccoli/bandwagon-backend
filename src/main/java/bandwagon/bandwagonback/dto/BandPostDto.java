ce89498502be7d75cf1122ca9f991102258b1923
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-07 11:12:54 +0900

    feat: Band Post에 들어가는 tag 수정

--- src/main/java/bandwagon/bandwagonback/dto/BandPostDto.java
+++ src/main/java/bandwagon/bandwagonback/dto/BandPostDto.java
@@ -2,8 +2,16 @@ package bandwagon.bandwagonback.dto;
 
 import bandwagon.bandwagonback.domain.User;
 import bandwagon.bandwagonback.domain.post.BandPost;
+import bandwagon.bandwagonback.domain.prerequisite.BandPrerequisite;
+import bandwagon.bandwagonback.domain.prerequisite.PositionPrerequisite;
+import bandwagon.bandwagonback.dto.subdto.IdNameForm;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -11,7 +19,7 @@ public class BandPostDto extends PostDto{
     private String bandName;
     private String bandAvatarUrl;
     //TODO: 나중엔 필요한 테그 정보만 담게 변경
-    private BandPageDto tagInfo;
+    private List<IdNameForm> tagInfo;
 
     public BandPostDto() {}
 
@@ -19,7 +27,12 @@ public class BandPostDto extends PostDto{
         super(post);
         this.bandName = post.getBand().getName();
         this.bandAvatarUrl = post.getBand().getAvatarUrl();
-        this.tagInfo = new BandPageDto(post.getBand(), false);
+        PositionPrerequisite positionPrereq = (PositionPrerequisite) post.getBandPrerequisites().stream().filter(prereq -> prereq.getDtype().equals("Position")).findFirst().orElse(null);
+        if (positionPrereq != null) {
+            this.tagInfo = positionPrereq.getPositions().stream().map(IdNameForm::new).collect(Collectors.toList());
+        } else {
+            this.tagInfo = new ArrayList<>();
+        }
     }
 
     public static BandPostDto makeBandPostDto(BandPost post, User user) {
