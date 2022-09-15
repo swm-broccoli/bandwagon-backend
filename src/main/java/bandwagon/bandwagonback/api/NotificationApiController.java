cb9fa7a840b3011c99ce0c2a6f8fc245baf63005
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-15 22:23:20 +0900

    refactor: AuthenticationPrincipal NotificationApiController에 적용

--- src/main/java/bandwagon/bandwagonback/api/NotificationApiController.java
+++ src/main/java/bandwagon/bandwagonback/api/NotificationApiController.java
@@ -3,7 +3,6 @@ package bandwagon.bandwagonback.api;
 import bandwagon.bandwagonback.domain.User;
 import bandwagon.bandwagonback.dto.NotificationListDto;
 import bandwagon.bandwagonback.dto.SimpleCountResponse;
-import bandwagon.bandwagonback.jwt.JwtUtil;
 import bandwagon.bandwagonback.service.NotificationService;
 import bandwagon.bandwagonback.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,11 +10,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Tag(name = "PostApiController")
 @Slf4j
@@ -26,13 +26,11 @@ public class NotificationApiController {
 
     private final NotificationService notificationService;
     private final UserService userService;
-    private final JwtUtil jwtTokenUtil;
 
     @Operation(description = "안읽은 알림 수 조회")
     @GetMapping("/api/notifications/count")
-    public ResponseEntity<?> getUnreadNotiCount(HttpServletRequest request) {
-        String jwt = getJwtFromHeader(request);
-        String email = jwtTokenUtil.extractUsername(jwt);
+    public ResponseEntity<?> getUnreadNotiCount(@AuthenticationPrincipal UserDetails userDetails) {
+        String email = userDetails.getUsername();
         User user = userService.findOneByEmail(email);
         long count = notificationService.getUnreadNotificationCount(user);
         return ResponseEntity.ok(new SimpleCountResponse(count));
@@ -40,16 +38,11 @@ public class NotificationApiController {
 
     @Operation(description = "알림 조회")
     @GetMapping("/api/notifications")
-    public ResponseEntity<?> getUserNotifications(HttpServletRequest request) {
-        String jwt = getJwtFromHeader(request);
-        String email = jwtTokenUtil.extractUsername(jwt);
+    public ResponseEntity<?> getUserNotifications(@AuthenticationPrincipal UserDetails userDetails) {
+        String email = userDetails.getUsername();
         User user = userService.findOneByEmail(email);
         NotificationListDto notificationListDto = notificationService.getNotificationToUser(user);
         return ResponseEntity.ok(notificationListDto);
     }
 
-    private String getJwtFromHeader(HttpServletRequest request) {
-        String authorizationHeader = request.getHeader("Authorization");
-        return authorizationHeader.substring(7);
-    }
 }
