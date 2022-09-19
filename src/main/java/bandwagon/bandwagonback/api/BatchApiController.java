a16c490430e958a466677e2dc6e4e4b4dba335b8
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-19 14:02:30 +0900

    feat: 추천 API에 secret key 조건 추가

--- src/main/java/bandwagon/bandwagonback/api/BatchApiController.java
+++ src/main/java/bandwagon/bandwagonback/api/BatchApiController.java
@@ -1,5 +1,6 @@
 package bandwagon.bandwagonback.api;
 
+import bandwagon.bandwagonback.dto.exception.notauthorized.UserNotAuthorizedException;
 import bandwagon.bandwagonback.service.RecommendationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Tag(name = "BatchApiController")
 @Slf4j
@@ -17,9 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BatchApiController {
 
     private final RecommendationService recommendationService;
+    // TODO: Change secret to be fetched from env var
+    private final String SECRET_KEY = "changetolocalvar";
 
     @PostMapping("/api/recommendations")
-    public ResponseEntity<?> makeRecommendations() {
+    public ResponseEntity<?> makeRecommendations(HttpServletRequest request) {
+        String requestKey = request.getHeader("Authorization");
+        if (!requestKey.equals(SECRET_KEY)) {
+            throw new UserNotAuthorizedException();
+        }
         recommendationService.makeRecommendations();
         return ResponseEntity.ok().body(null);
     }
