a026afde63cdd22e440a9f72b0cbf4e93d060c88
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-19 11:08:08 +0900

    feat: 추천 메서드 틀 구현

--- src/main/java/bandwagon/bandwagonback/service/RecommendationService.java
+++ src/main/java/bandwagon/bandwagonback/service/RecommendationService.java
@@ -1,10 +1,16 @@
 package bandwagon.bandwagonback.service;
 
+import bandwagon.bandwagonback.domain.User;
+import bandwagon.bandwagonback.domain.post.BandPost;
+import bandwagon.bandwagonback.repository.BandPostRepository;
 import bandwagon.bandwagonback.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,4 +19,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class RecommendationService {
 
     private final UserRepository userRepository;
+    private final BandPostRepository bandPostRepository;
+    private final BandPrerequisiteService bandPrerequisiteService;
+
+    @Transactional
+    public void makeRecommendations() {
+        List<User> allUsers = userRepository.findAll();
+        List<BandPost> allBandPosts = bandPostRepository.findAll();
+        for (User user : allUsers) {
+            user.resetRecommendation();
+            HashMap<Long, Integer> scoreByPost = new HashMap<>();
+            // scorebypost에 bandpost별 점수 추가
+
+            while(user.getRecommendedPosts().size() < 3) {
+                // scorebypost 정렬 후 top 3개 추가
+            }
+        }
+    }
 }
