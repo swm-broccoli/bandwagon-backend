666051b1a1a3508cc55623122725d638a7d80168
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2022-09-20 16:54:03 +0900

    refactor: 추천 게시글 4개로 변경

--- src/main/java/bandwagon/bandwagonback/service/RecommendationService.java
+++ src/main/java/bandwagon/bandwagonback/service/RecommendationService.java
@@ -40,7 +40,7 @@ public class RecommendationService {
             LinkedList<Map.Entry<BandPost, Integer>> scoreList = new LinkedList<>(scoreByPost.entrySet());
             scoreList.sort(((o1, o2) -> o2.getValue() - o1.getValue()));
             int i = 0;
-            while(user.getRecommendedPosts().size() < 3 && i < scoreList.size()) {
+            while(user.getRecommendedPosts().size() < 4 && i < scoreList.size()) {
                 user.addRecommendation(scoreList.get(i).getKey());
                 i++;
             }
